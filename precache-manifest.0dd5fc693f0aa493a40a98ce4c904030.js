0a607d2ea5e28f3d6389d0c64986ef0676ea3c81
Fred Malm <fredrik.malmborg@medieinstitutet.se>
2020-08-28 14:25:27 +0200

    Updates

---
+++ precache-manifest.0dd5fc693f0aa493a40a98ce4c904030.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3722e9d540310b324673109aca00b02a",
+    "url": "/heresy/index.html"
+  },
+  {
+    "revision": "b15c5f4e3e9ef777f8d8",
+    "url": "/heresy/static/css/main.60a5f816.chunk.css"
+  },
+  {
+    "revision": "77d04415f5b2a0e7d864",
+    "url": "/heresy/static/js/2.029323c4.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/heresy/static/js/2.029323c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b15c5f4e3e9ef777f8d8",
+    "url": "/heresy/static/js/main.6b29c603.chunk.js"
+  },
+  {
+    "revision": "a3f084b684292874b1c0",
+    "url": "/heresy/static/js/runtime-main.ffd6c362.js"
+  },
+  {
+    "revision": "51a626397485b6adfae5590e94d5d461",
+    "url": "/heresy/static/media/PHOSS_.51a62639.ttf"
+  },
+  {
+    "revision": "b2c5da732f1451ebb9c7dc8b14aefccc",
+    "url": "/heresy/static/media/cross.b2c5da73.svg"
+  },
+  {
+    "revision": "a8837da574b4021ca963df47679b5132",
+    "url": "/heresy/static/media/heresy_logo_emblem.a8837da5.svg"
+  },
+  {
+    "revision": "050bc33be9384d12414c493000ddd095",
+    "url": "/heresy/static/media/heresy_logo_header.050bc33b.svg"
+  }
+]);
